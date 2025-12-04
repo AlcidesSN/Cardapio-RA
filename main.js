f4045eebe7e3414cdd6d3d2eba911a402bd86d62
Alcides Sandoli Neto <alcidessandoli@edu.unifil.br>
2025-12-04 11:00:56 -0300

    asjuste de escala

--- main.js
+++ main.js
@@ -7,56 +7,64 @@ const menuItems = [
         nome: "Frango Assado com Ervas",
         preco: 54,
         descricao: "Frango que não é piru.",
-        modelo3D: "./modelos/Roasted_Thanksgiving_Turkey_Prop_t.glb"
+        modelo3D: "./modelos/Roasted_Thanksgiving_Turkey_Prop_t.glb",
+        scala: "3 3 3"
     },
     {
         categoria: "Sobremesas",
         nome: "Torta de Pessego",
         preco: 22,
         descricao: "Torta com pêssegos frescos.",
-        modelo3D: "./modelos/pumpkin_texture.glb"
+        modelo3D: "./modelos/pumpkin_texture.glb",
+        scala: "7 7 7"
     },
     {
         categoria: "Bebidas",
         nome: "Chá Gelado de Pêssego",
         preco: 12,
         descricao: "Cha gelado.",
-        modelo3D: "./modelos/tea.glb"
+        modelo3D: "./modelos/tea.glb",
+        scala: "0.4 0.4 0.4"
     },
     {
         categoria: "Entradas",
         nome: "Lich Monarch",
         preco: 0,
         descricao: "Morto vivo muito poderoso.",
-        modelo3D: "./modelos/Dark_Lich_Monarch_1001225807_texture.glb"
+        modelo3D: "./modelos/Dark_Lich_Monarch_1001225807_texture.glb",
+        scala: "1 1 1"
     },
     {
         categoria: "Entradas",
         nome: "Mini Sanduíches",
         preco: 0,
         descricao: "Mini sanduíches variados.",
-        modelo3D: "./modelos/plate_with_small_sandwitches.glb"
+        modelo3D: "./modelos/sandwiches_pack.glb",
+        scala: "7 7 7"
     },
     {
         categoria: "Pratos Principais",
         nome: "PF de Bacon",
         preco: 0,
         descricao: "Prato feito com bacon.",
-        modelo3D: "./modelos/pratofeitobacon.glb"
+        modelo3D: "./modelos/pratofeitobacon.glb",
+        scala: "1 1 1"
     },
     {
         categoria: "Sobremesas",
         nome: "Brownies com sorvete",
         preco: 0,
         descricao: "3 Brownie e uma bola de sorvete de creme.",
-        modelo3D: "./modelos/plate_of_brownies.glb"
+        modelo3D: "./modelos/plate_of_brownies.glb",
+        scala: "150 150 150"
     },
         {
         categoria: "Sobremesas",
         nome: "Pudim",
         preco: 0,
         descricao: "Pudim de leite.",
-        modelo3D: "./modelos/typical_brazilian_glass_plate_free.glb"
+        modelo3D: "./modelos/typical_brazilian_glass_plate_free.glb", //sem textura
+        scala: "3 3 3"
     },
 
 ];
@@ -91,7 +99,7 @@ function gerarMenu() {
             div.innerHTML = `
                         <div class="item-header">
                             <div class="item-name">${item.nome}</div>
-                            <button class="view-btn" onclick="carregarModelo('${item.modelo3D}')">👁️</button>
+                            <button class="view-btn" onclick="carregarModelo('${item.modelo3D}', '${item.scala}')">👁️</button>
                         </div>
                         <div class="item-description">${item.descricao}</div>
                         <div class="item-price">R$ ${item.preco},00</div>
@@ -109,7 +117,7 @@ gerarMenu();
 /********************************************
  * ALTERAR MODELO 3D DINAMICAMENTE          *
  ********************************************/
-function carregarModelo(caminho) {
+function carregarModelo(caminho, scala) {
     console.log("Carregando modelo:", caminho);
     const marker = document.querySelector("a-marker");
     const antigo = document.getElementById("model");
@@ -117,13 +125,11 @@ function carregarModelo(caminho) {
     // Remove o modelo antigo
     if (antigo) antigo.remove();
 
-    // Cria o novo modelo DENTRO DO MARKER
     const novo = document.createElement("a-entity");
     novo.setAttribute("id", "model");
     novo.setAttribute("gltf-model", caminho);
 
-    // Ajustes para evitar câmera preta
-    novo.setAttribute("scale", "1 1 1");
+    novo.setAttribute("scale", scala);
     novo.setAttribute("rotation", "-90 0 0");
     novo.setAttribute("position", "0 0 0");
 
